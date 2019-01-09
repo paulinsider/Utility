2b43ef870bbb16c822e674a41e2c55fe60673ce9
huben <huben@youhaodongxi.com>
2019-01-09 15:39:30 +0800

    update app

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const goodItem = 354;
 // 2
 // 3
 
+const swipeLeft = `adb shell input swipe ${x - 100} ${header + goodItem} ${x - 400} ${header + goodItem}`
+
 const swipeTop50 = `adb shell input swipe ${x/2} ${bigHeader + goodItem} ${x/2} ${header + goodItem}`
 
 const tap1 = `adb shell input tap ${x/4} ${header + goodItem/2}`
@@ -22,11 +24,10 @@ const back = 'adb shell input keyevent 4'
 
 const tap2 = `adb shell input tap ${x/4*3} ${header + goodItem/2}`
 
-const swipeItem = `adb shell input swipe ${x/2} ${header + goodItem * 2} ${x/2} ${header + goodItem + 13} 500`
+const swipeItem = `adb shell input swipe ${x/2} ${bigHeader + goodItem * 2} ${x/2} ${bigHeader + goodItem} 500`
 
-async function start() {
-  process.execSync(swipeTop50)
-  await sleep()
+async function loop() {
+  
   process.execSync(tap1)
   await sleep()
   process.execSync(back)
@@ -36,7 +37,7 @@ async function start() {
   process.execSync(back)
   await sleep()
   process.execSync(swipeItem)
-
+  await sleep()
 }
 
 async function sleep(delay = 1500) {
@@ -48,4 +49,20 @@ async function sleep(delay = 1500) {
   
 }
 
-start()
+
+(async function() {
+  const connectResponse = process.execSync('adb connect 127.0.0.1:62001')
+  console.log(connectResponse.toString())
+
+  const devicesResponse = process.execSync('adb devices')
+  console.log(devicesResponse.toString())
+
+  process.execSync('adb shell am start -n com.shizhuang.duapp/com.shine.ui.home.SplashActivity')
+  await sleep(6000)
+
+  process.execSync(swipeLeft)
+  // process.execSync(swipeTop50)
+  for(;;) {
+    await loop()
+  }
+})()
